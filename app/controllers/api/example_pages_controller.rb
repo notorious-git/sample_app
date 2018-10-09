f89f20ed6b56ea18e2efa6cb1b463ceebe36f54c
Peter Jang <peter.x.jang@gmail.com>
2018-10-09 18:57:22 -0500

    Add dynamic example with strftime

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -2,4 +2,12 @@ class Api::ExamplePagesController < ApplicationController
   def hello_method
     render json: {message: "yo!!!"}
   end
+
+  def peter
+    render json: {test: "hi"}
+  end
+
+  def time_method
+    render json: {time: Time.now.strftime("%b %e, %l:%M %p")}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/hello_url" => "api/example_pages#hello_method"
+  get "/peter" => "api/example_pages#peter"
+  get "/time_url" => "api/example_pages#time_method"
 end
